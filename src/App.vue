499d228303a7d2df3b5c9898ec48ae9f54e265db
nmadnorangen <caesgou@mail.ru>
2026-01-13 13:31:24 +0500

    trying to resolve publication error

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import router from "./router";
+
+function to_home() {
+  return window.scrollTo({ top: 0, behavior: "smooth" });
+}
 </script>
 
 <template>
@@ -12,7 +16,14 @@ import router from "./router";
   </header>
 
   <RouterView />
-  <!-- <footer></footer> -->
+  <footer>
+    <nav>
+      <RouterLink to="/">Товары</RouterLink>
+      <RouterLink to="/cart">Корзина</RouterLink>
+    </nav>
+    <button class="button-to-home" @click="to_home">↑В начало↑</button>
+    <p>По вопросам: caesgou@mail.ru</p>
+  </footer>
 </template>
 
 <style scoped></style>
